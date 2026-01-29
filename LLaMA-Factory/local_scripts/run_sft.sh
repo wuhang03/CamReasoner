17c0e3241d2f0c2ccc66e8698eff050729790227
wuhang03 <wuh492465@gmail.com>
2026-01-29 01:56:06 +0000

    update sft

--- LLaMA-Factory/local_scripts/run_sft.sh
+++ LLaMA-Factory/local_scripts/run_sft.sh
@@ -4,4 +4,4 @@ export DECORD_EOF_RETRY_MAX=2048001
 
 
 # llamafactory-cli train LLaMA-Factory/examples/train_full/onethinker_qwen3_sft.yaml
-llamafactory-cli train examples/train_full/camreaonser_sft.yaml
+llamafactory-cli train examples/train_full/camreasoner_sft.yaml
